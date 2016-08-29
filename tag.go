7221d62d36d4d0160364ccab6f721df6bf43c87d
im7mortal <im7mortal@gmail.com>
2016-08-29 18:15:05 +0300

    added tag_t model struct
    all test is passed

---
+++ tag.go
@@ -0,0 +1,19 @@
+package obj
+
+/*
+typedef struct {
+  std::string name;
+
+  std::vector<int> intValues;
+  std::vector<float> floatValues;
+  std::vector<std::string> stringValues;
+} tag_t;
+*/
+
+type TagT struct {
+	Name         string
+
+	IntValues    []int
+	FloatValues  []float64
+	StringValues []string
+}
