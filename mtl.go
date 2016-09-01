90ba7414f14dd9419b5e6caf6053403153fdc028
im7mortal <im7mortal@gmail.com>
2016-09-01 21:32:28 +0300

    mtl parser is done for now
    all test is passed

--- mtl.go
+++ mtl.go
@@ -1,5 +1,11 @@
 package obj
 
+import (
+	"io"
+	"bufio"
+	"bytes"
+)
+
 /*
 typedef struct {
   std::string name;
@@ -85,4 +91,193 @@ type Mtl struct {
 
 
 	UnknownParameter          map[string]string
+}
+
+func LoadMtl(r io.Reader) *[]Mtl {
+
+	materials := []Mtl{}
+	var currentMtl Mtl
+	first := false
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanLines)
+	var token []byte
+	var res [][]byte
+	for scanner.Scan() {
+		token = bytes.Trim(scanner.Bytes(), " ")
+		switch {
+		case len(token) == 0 || bytes.ContainsRune(token, '#'):
+		// new mtl
+		case string(token[:6]) == "newmtl" && isSpace(token[6]) :
+			if first {
+				materials = append(materials, currentMtl)
+			}
+			first = true
+			currentMtl = newMtl()
+			currentMtl.Name = string(token[7:])
+
+
+		// ambient
+		case token[0] == 'K' && token[1] == 'a' && isSpace(token[2]) :
+			res = bytes.Split(token[3:], WSS)
+			currentMtl.Ambient = parseRGB(res)
+		// diffuse
+		case token[0] == 'K' && token[1] == 'd' && isSpace(token[2]) :
+			res = bytes.Split(token[3:], WSS)
+			currentMtl.Diffuse = parseRGB(res)
+		// specular
+		case token[0] == 'K' && token[1] == 's' && isSpace(token[2]) :
+			res = bytes.Split(token[3:], WSS)
+			currentMtl.Specular = parseRGB(res)
+		// transmittance
+		case token[0] == 'K' && token[1] == 't' && isSpace(token[2]) ||
+			token[0] == 'T' && token[1] == 'f' && isSpace(token[2]) :
+			res = bytes.Split(token[3:], WSS)
+			currentMtl.Transmittance = parseRGB(res)
+		// emission
+		case token[0] == 'K' && token[1] == 'e' && isSpace(token[2]) :
+			res = bytes.Split(token[3:], WSS)
+			currentMtl.Emission = parseRGB(res)
+		// ior(index of refraction)
+		case token[0] == 'N' && token[1] == 'i' && isSpace(token[2]) :
+			currentMtl.Ior = parseFloat(string(token[3:]))
+		// shininess
+		case token[0] == 'N' && token[1] == 's' && isSpace(token[2]) :
+			currentMtl.Shininess = parseFloat(string(token[3:]))
+		// illum model
+		case string(token[0:5]) == "illum" && isSpace(token[5]) :
+			currentMtl.Illum = parseInt(string(token[6:]))
+		// dissolve
+		case token[0] == 'd' && isSpace((token[1])) :
+			currentMtl.Dissolve = parseFloat(string(token[2:]))
+		case token[0] == 'T' && token[1] == 'r' && isSpace(token[2]) :
+			// Invert value of Tr(assume Tr is in range [0, 1])
+			currentMtl.Shininess = 1. - parseFloat(string(token[3:]))
+		// PBR: roughness
+		case token[0] == 'P' && token[1] == 'r' && isSpace((token[2])) :
+			currentMtl.Roughness = parseFloat(string(token[3:]))
+		// PBR: metallic
+		case token[0] == 'P' && token[1] == 'm' && isSpace((token[2])) :
+			currentMtl.Metallic = parseFloat(string(token[3:]))
+		// PBR: sheen
+		case token[0] == 'P' && token[1] == 's' && isSpace((token[2])) :
+			currentMtl.Sheen = parseFloat(string(token[3:]))
+		// PBR: clearcoat thickness
+		case token[0] == 'P' && token[1] == 'c' && isSpace((token[2])) :
+			currentMtl.ClearcoatThickness = parseFloat(string(token[3:]))
+		// PBR: clearcoat roughness
+		case string(token[:3]) == "Pcr" && isSpace(token[3]) :
+			currentMtl.ClearcoatRoughness = parseFloat(string(token[4:]))
+		// PBR: anisotropy
+		case string(token[:5]) == "aniso" && isSpace(token[5]) :
+			currentMtl.Anisotropy = parseFloat(string(token[6:]))
+		// PBR: anisotropy rotation
+		case string(token[:6]) == "anisor" && isSpace(token[6]) :
+			currentMtl.AnisotropyRotation = parseFloat(string(token[7:]))
+		// ambient texture
+		case string(token[:6]) == "map_Ka" && isSpace(token[6]) :
+			currentMtl.AmbientTextName = string(token[7:])
+		// diffuse texture
+		case string(token[:6]) == "map_Kd" && isSpace(token[6]) :
+			currentMtl.DiffuseTextName = string(token[7:])
+		// specular texture
+		case string(token[:6]) == "map_Ks" && isSpace(token[6]) :
+			currentMtl.SpecularTextName = string(token[7:])
+		// specular highlight texture
+		case string(token[:6]) == "map_Ns" && isSpace(token[6]) :
+			currentMtl.SpecularHighlightTextName = string(token[7:])
+		// alpha texture
+		case string(token[:5]) == "map_d" && isSpace(token[5]) :
+			currentMtl.AlphaTextName = string(token[6:])
+		// bump texture
+		case string(token[:7]) == "map_bump" && isSpace(token[7]) :
+			currentMtl.BumpTextName = string(token[8:])
+		case string(token[:4]) == "bump" && isSpace(token[4]) :
+			currentMtl.BumpTextName = string(token[5:])
+		// displacement texture
+		case string(token[:4]) == "disp" && isSpace(token[4]) :
+			currentMtl.DisplacementTextName = string(token[5:])
+		// PBR: roughness texture
+		case string(token[:6]) == "map_Pr" && isSpace(token[6]) :
+			currentMtl.RoughnessTextName = string(token[7:])
+		// PBR: metallic texture
+		case string(token[:6]) == "map_Pm" && isSpace(token[6]) :
+			currentMtl.MetallicTextName = string(token[7:])
+		// PBR: sheen texture
+		case string(token[:6]) == "map_Ps" && isSpace(token[6]) :
+			currentMtl.SheenTextName = string(token[7:])
+		// PBR: emissive texture
+		case string(token[:6]) == "map_Ke" && isSpace(token[6]) :
+			currentMtl.EmissiveTextName = string(token[7:])
+		// PBR: normal map texture
+		case string(token[:4]) == "norm" && isSpace(token[4]) :
+			currentMtl.NormalTextName = string(token[5:])
+		// unknown parameter
+		default:
+			if currentMtl.UnknownParameter == nil {
+				currentMtl.UnknownParameter = map[string]string{}
+			}
+			i := bytes.IndexAny(token, " \t")
+			// if it doesn't has parameters. put entire one
+			if i == -1 {
+				currentMtl.UnknownParameter[string(token)] = string(token)
+			} else {
+				currentMtl.UnknownParameter[string(token[:i - 1])] = string(string(token[i:]))
+			}
+		}
+	}
+	materials = append(materials, currentMtl)
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return &materials
+}
+
+func newMtl() Mtl {
+	mtl := Mtl{}
+
+	mtl.Name = ""
+
+	mtl.AmbientTextName = ""
+	mtl.DiffuseTextName = ""
+	mtl.SpecularTextName = ""
+	mtl.SpecularHighlightTextName = ""
+	mtl.BumpTextName = ""
+	mtl.DisplacementTextName = ""
+	mtl.AlphaTextName = ""
+
+
+		mtl.Ambient = [3]float64{}
+		mtl.Diffuse = [3]float64{}
+		mtl.Specular = [3]float64{}
+		mtl.Transmittance = [3]float64{}
+		mtl.Emission = [3]float64{}
+
+	for i := 0; i < 3; i++ {
+		mtl.Ambient[i] = 0.
+		mtl.Diffuse[i] = 0.
+		mtl.Specular[i] = 0.
+		mtl.Transmittance[i] = 0.
+		mtl.Emission[i] = 0.
+	}
+
+	mtl.Shininess = 1.
+	mtl.Ior = 1.
+	mtl.Dissolve = 1.
+	mtl.Illum = 0.
+
+	mtl.Roughness = 0.
+	mtl.Metallic = 0.
+	mtl.Sheen = 0.
+	mtl.ClearcoatThickness = 0.
+	mtl.ClearcoatRoughness = 0.
+	mtl.Anisotropy = 0.
+	mtl.AnisotropyRotation = 0.
+
+	mtl.RoughnessTextName = ""
+	mtl.MetallicTextName = ""
+	mtl.SheenTextName = ""
+	mtl.EmissiveTextName = ""
+	mtl.NormalTextName = ""
+
+	return mtl
 }
--- mtl_test.go
+++ mtl_test.go
@@ -2,10 +2,92 @@ package obj
 
 import (
 	"testing"
+	"reflect"
+	"bytes"
 )
 
-func TestParseMtl(t *testing.T) {
-	if false {
-		t.Error("it's not possible")
+var expectedMtl Mtl = Mtl{
+	Name : "",
+
+	AmbientTextName : "",
+	DiffuseTextName : "",
+	SpecularTextName : "",
+	SpecularHighlightTextName : "",
+	BumpTextName : "",
+	DisplacementTextName : "",
+	AlphaTextName : "",
+
+		Ambient : [3]float64{0., 0., 0.},
+		Diffuse : [3]float64{0., 0., 0.},
+		Specular : [3]float64{0., 0., 0.},
+		Transmittance : [3]float64{0., 0., 0.},
+		Emission : [3]float64{0., 0., 0.},
+
+	Shininess : 1.,
+	Ior : 1.,
+	Dissolve : 1.,
+	Illum : 0.,
+
+	Roughness : 0.,
+	Metallic : 0.,
+	Sheen : 0.,
+	ClearcoatThickness : 0.,
+	ClearcoatRoughness : 0.,
+	Anisotropy : 0.,
+	AnisotropyRotation : 0.,
+
+	RoughnessTextName : "",
+	MetallicTextName : "",
+	SheenTextName : "",
+	EmissiveTextName : "",
+	NormalTextName : "",
+}
+
+func TestNewMtl(t *testing.T) {
+	have := newMtl()
+	if !reflect.DeepEqual(expectedMtl, have) {
+		println("parseRGB() returns a bad result")
+		println("We want")
+		DEBUG(expectedMtl)
+		println("But we have")
+		DEBUG(have)
+		t.Error("")
 	}
 }
+
+var testMtl = `# Blender MTL File: 'None'
+# Material Count: 1
+
+newmtl None
+Ns 0
+Ka 0.000000 0.000000 0.000000
+Kd 0.8 0.8 0.8
+Ks 0.8 0.8 0.8
+d 1
+illum 2
+`
+
+func TestParseMtl(t *testing.T) {
+	mtl := newMtl()
+	mtl.Name = "None"
+	mtl.Shininess = 0
+	mtl.Ambient = [3]float64{0., 0., 0.}
+	mtl.Diffuse = [3]float64{0.8, 0.8, 0.8}
+	mtl.Specular = [3]float64{0.8, 0.8, 0.8}
+	mtl.Dissolve = 1
+	mtl.Illum = 2
+	want_ := []Mtl{mtl}
+	want := &want_
+
+	have := LoadMtl(bytes.NewBufferString(testMtl))
+
+	if !reflect.DeepEqual(want, have) {
+		println("parseRGB() returns a bad result")
+		println("We want")
+		DEBUG(want)
+		println("But we have")
+		DEBUG(have)
+		t.Error("")
+	}
+}
+
