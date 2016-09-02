07cdde80d73c51e46996af13773a38dff34854f5
im7mortal <im7mortal@gmail.com>
2016-09-02 23:16:28 +0300

    new tests for utils
    tests passed

--- utils_test.go
+++ utils_test.go
@@ -7,24 +7,31 @@ import (
 	"reflect"
 )
 
-
-
 func TestParseRGB(t *testing.T) {
 	kd := []byte("Kd 0.8 0.8 0.8")
 	arr := bytes.Split(kd[3:], []byte(" "))
 	have := parseRGB(arr)
 	want := [3]float64{0.8, 0.8, 0.8}
 	if !reflect.DeepEqual(want, have) {
-		println("parseRGB() returns a bad result")
-		println("We want")
-		DEBUG(want)
-		println("But we have")
-		DEBUG(have)
-		t.Error("")
+		t.Error("parseRGB() returns a bad result")
 	}
 }
 
+func TestParseFloat (t *testing.T) {
+	have := parseFloat("0.8")
+	want := 0.8
+	if have != want {
+		t.Error("parseFloat returns a bad result")
+	}
+}
 
+func TestParseInt (t *testing.T) {
+	have := parseInt("255")
+	want := 255
+	if have != want {
+		t.Error("parseInt returns a bad result")
+	}
+}
 
 func DEBUG(i interface{})  {
 	fmt.Printf("%f\n", i)
